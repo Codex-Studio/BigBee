df720b197e5c86ee593f10cbd584f3a3ff2a9c65
Toktorov <ktoktorov2005@gmail.com>
2023-05-30 15:13:12 +0600

    add user model and add registerion

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    USER_ROLE_CHOICE = (
+        ('Client', 'Client'),
+        ('Partner', 'Partner')
+    )
+    user_role = models.CharField(
+        max_length=100,
+        choices=USER_ROLE_CHOICE,
+        verbose_name="Роль пользователя",
+        default='Client'
+    )
+    promo_code = models.CharField(
+        max_length=100,
+        blank=True, null=True
+    )
+    
+    def __str__(self):
+        return self.username 
+    
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+
+from apps.settings.models import Setting
+from apps.users.models import User
 
 # Create your views here.
+def register(request):
+    setting = Setting.objects.latest('id')
+    if request.method == "POST":
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = User.objects.create(username=email, email=email)
+        user.set_password(password)
+        user.save()
+        return redirect('index')
+    return render(request, 'users/register.html', locals())
